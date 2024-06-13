8805bb4ed289f0d06ac1755ebf035400a08dd67e
ryan378 <42106060+ryan378@users.noreply.github.com>
2024-06-13 15:41:49 +0700

    wise word

---
+++ my-wise-word-compose/wise-word/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/gofiber/fiber/v3"
+	_ "github.com/lib/pq"
+)
+
+func main() {
+	// Membaca variabel lingkungan
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASS")
+	dbName := os.Getenv("DB_NAME")
+
+	// Membuat string koneksi database
+	connStr := fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=disable",
+		dbUser, dbPassword, dbHost, dbPort, dbName)
+
+	fmt.Println(connStr)
+
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		fmt.Println("Connect db failed")
+	}
+
+	// Membuat aplikasi Fiber
+	app := fiber.New()
+
+	// Rute untuk "/"
+	app.Get("/", func(c fiber.Ctx) error {
+		fmt.Println("ouch!")
+		return c.SendString("<html><body><h1>Manusia Hanyalah Alat!</h1></body></html>")
+	})
+
+	// Rute untuk "/ping"
+	app.Get("/ping", func(c fiber.Ctx) error {
+		if err := db.Ping(); err != nil {
+			log.Printf("Gagal mendapatkan koneksi: %v\n", err)
+			return c.SendString("Connection to database failed")
+		}
+
+		return c.SendString("Success connect database")
+	})
+
+	// Memulai server
+	app.Listen(":78")
+}
